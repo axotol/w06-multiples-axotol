fb79e83ef83b2d103dc0920ee85b0db9fddc5088
Melanie <102451625+axotol@users.noreply.github.com>
2022-09-19 23:23:32 +1000

    Completed step 2 in "More intents and fragments"

--- app/src/main/java/com/example/w06_multiples_axotol/MainActivity.kt
+++ app/src/main/java/com/example/w06_multiples_axotol/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvFactor1 = findViewById<TextView>(R.id.factor1)
-        val tvFactor2 = findViewById<TextView>(R.id.factor2)
+        val tvFactorLayout1 = findViewById<TextInputLayout>(R.id.tilFactor1)
+        val tvFactor1 = findViewById<TextInputEditText>(R.id.factor1)
+        val tvFactorLayout2 = findViewById<TextInputLayout>(R.id.tilFactor2)
+        val tvFactor2 = findViewById<TextInputEditText>(R.id.factor2)
         val multiply = findViewById<Button>(R.id.btnMultiply)
 
-        val factor1 = Random.nextInt(from = 1, until = 13)
-        val factor2 = Random.nextInt(from = 1, until = 13)
+        //val factor1 = Random.nextInt(from = 1, until = 13)
+        //val factor2 = Random.nextInt(from = 1, until = 13)
 
-        tvFactor1.text = factor1.toString()
-        tvFactor2.text = factor2.toString()
+        //tvFactor1.text = factor1.toString()
+        //tvFactor2.text = factor2.toString()
 
         multiply.setOnClickListener {
             /* step 2
@@ -35,12 +39,44 @@ class MainActivity : AppCompatActivity() {
             }
              */
 
-            // step 4
-            val i = Intent(this, ResultActivity::class.java).apply {
-                putExtra("result", Result(factor1 * factor2))
+            var proceed: Boolean = true
+
+            var factor1 = -1
+            var factor2 = -1
+
+            if (tvFactor1.text.toString() == "") {
+                tvFactor1.error = "Please enter a number" // or can use tvFactorLayout1
+                proceed = false
+            } else {
+                factor1 = tvFactor1.text.toString().toInt()
+                if (factor1 < 1 || factor1 > 12) {
+                    tvFactor1.error = "Value must be between 1-12"
+                    proceed = false
+                } else {
+                    tvFactor1.error = null
+                }
             }
 
-            startActivity(i)
+            if (tvFactor2.text.toString() == "") {
+                tvFactor2.error = "Please enter a number"
+                proceed = false
+            } else {
+                factor2 = tvFactor2.text.toString().toInt()
+                if (factor2 < 1 || factor2 > 12) {
+                    tvFactor2.error = "Value must be between 1-12"
+                    proceed = false
+                } else {
+                    tvFactor2.error = null
+                }
+            }
+
+            if (proceed) {
+                // step 4
+                val i = Intent(this, ResultActivity::class.java).apply {
+                    putExtra("result", Result(factor1 * factor2))
+                }
+                startActivity(i)
+            }
         }
     }
 }
